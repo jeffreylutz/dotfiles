ce56006c3f9f74f9347f2145698fc088e7fc81ca
Jeffrey Lutz <jeff@autonomi-inc.com>
2024-11-26 18:29:37 -0500

    Added sha256 for remote.it

--- nix/darwin/pkgs/remoteit/default.nix
+++ nix/darwin/pkgs/remoteit/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "remoteit";
-  version = "3.19.7"; # Update this to the latest version
+  version = "3.34.4"; # Update this to the latest version
 
 https://downloads.remote.it/desktop/v3.34.4/Remote.It-Installer-arm64.dmg
 
   src = fetchurl {
     url = "https://downloads.remote.it/desktop/v3.34.4/Remote.It-Installer-arm64.dmg";
-    sha256 = ""; # You'll need to add the actual sha256 after downloading the file
+    sha256 = "1gwsdzvi506wfxphnnq2nipvdmbh8qyc9xf7qigzqpp2418gkw05"; # You'll need to add the actual sha256 after downloading the file
   };
 
   nativeBuildInputs = [ undmg ];
