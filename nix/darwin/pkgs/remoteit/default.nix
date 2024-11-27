8e2839c24d43e8e5433ae34d9b6b01830fe8c298
Jeffrey Lutz <jeff@autonomi-inc.com>
2024-11-26 19:05:58 -0500

    Fixed stuff remote-it

--- nix/darwin/pkgs/remoteit/default.nix
+++ nix/darwin/pkgs/remoteit/default.nix
@@ -4,8 +4,6 @@ stdenv.mkDerivation rec {
   pname = "remoteit";
   version = "3.34.4"; # Update this to the latest version
 
-https://downloads.remote.it/desktop/v3.34.4/Remote.It-Installer-arm64.dmg
-
   src = fetchurl {
     url = "https://downloads.remote.it/desktop/v3.34.4/Remote.It-Installer-arm64.dmg";
     sha256 = "1gwsdzvi506wfxphnnq2nipvdmbh8qyc9xf7qigzqpp2418gkw05"; # You'll need to add the actual sha256 after downloading the file
